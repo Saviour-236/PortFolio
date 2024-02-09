6abbb4415785190120f8fdeaf61aaf45894faacd
Suresh <thakursureshkumar118@gmail.com>
2024-02-09 16:26:05 +0530

    deleted

--- out/_next/static/chunks/app/Contacts/loading-0a1b09aca19fc4ab.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[20,160,587,99,990,623,430,510,378,601],{3521:function(){}},function(n){n.O(0,[971,69,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
--- out/_next/static/chunks/app/error-e8bcd5d63e475d3a.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[424],{2822:function(n,e,r){Promise.resolve().then(r.bind(r,449))},449:function(n,e,r){"use strict";r.r(e);var u=r(3827);r(4090),e.default=function(){return(0,u.jsx)("div",{children:"error"})}}},function(n){n.O(0,[971,69,744],function(){return n(n.s=2822)}),_N_E=n.O()}]);
