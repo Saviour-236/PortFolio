85c9f159ee0273e1874696893d025dafefabbfe6
Suresh <thakursureshkumar118@gmail.com>
2024-02-22 12:12:21 +0530

    doing again

--- out/_next/static/chunks/app/not-found-8cfdba12c3a7f8de.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[160,623,396,89,378,20,99,192,302],{3521:function(){}},function(n){n.O(0,[971,69,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
--- src/app/largeComponents/footer.tsx
+++ src/app/largeComponents/footer.tsx
@@ -3,23 +3,24 @@ import React,{useState} from 'react'
 import { CiCircleChevDown } from "react-icons/ci";
 import Link from 'next/link'
 function footer() {
-  const [buttons,setbuttons] =useState([{name:<p >Project</p>,link:'/Projects',cls:'  bg-black h-fit rounded p-[0.3rem]  m-[1rem]   flex p-[0.6rem] items-center space-x-[1rem] w-fit  hover:shadow-lg shadow-md hover:shadow-[#d9d6d0] shadow-[#d9d6d0] transition ease-in-out delay-50   hover:-translate-y-1 hover:scale-110   '},
-  {name:<p>Skills</p>,link:'/Skills',cls:'h-fit bg-black rounded p-[0.5rem]  m-[1rem]  w-[5rem] flex p-[0.6rem] items-center space-x-[1rem] w-fit   hover:shadow-lg shadow-md hover:shadow-[#d9d6d0] shadow-[#d9d6d0] transition ease-in-out delay-50   hover:-translate-y-1 hover:scale-110  '},
-  {name:<p>Experience</p>,link:'/Experience',cls:'h-fit rounded p-[0.5rem]  bg-black  m-[1rem]  w-[5rem] flex p-[0.6rem] items-center space-x-[1rem] w-fit   hover:shadow-lg shadow-md hover:shadow-[#d9d6d0] shadow-[#d9d6d0] transition ease-in-out delay-50   hover:-translate-y-1 hover:scale-110  '
-  },
-  {name:<p>Resume</p>,link:'/Resume',cls:'h-fit rounded p-[0.5rem]  m-[1rem] bg-black  w-[5rem] flex p-[0.6rem] items-center space-x-[1rem] w-fit   hover:shadow-lg shadow-md hover:shadow-[#d9d6d0] shadow-[#d9d6d0] transition ease-in-out delay-50   hover:-translate-y-1 hover:scale-110  '},
-  {name:<p>Contacts</p>,link:'/Contacts',cls:'h-fit rounded p-[0.5rem]  m-[1rem]  bg-black  w-[5rem] flex p-[0.6rem] items-center space-x-[1rem] w-fit   hover:shadow-lg shadow-md hover:shadow-[#d9d6d0] shadow-[#d9d6d0] transition ease-in-out delay-50   hover:-translate-y-1 hover:scale-110  '}])
+  const [buttons,setbuttons] =useState([
+    {name:'Project',link:'/Projects',cls:'  bg-black h-fit rounded p-[0.3rem]  m-[1rem]   flex p-[0.6rem] items-center space-x-[1rem] w-fit  hover:shadow-lg shadow-md hover:shadow-[#d9d6d0] shadow-[#d9d6d0] transition ease-in-out delay-50   hover:-translate-y-1 hover:scale-110   '},
+   {name:'Skills',link:'/Skills',cls:'h-fit bg-black rounded p-[0.5rem]  m-[1rem]  w-[5rem] flex p-[0.6rem] items-center space-x-[1rem] w-fit   hover:shadow-lg shadow-md hover:shadow-[#d9d6d0] shadow-[#d9d6d0] transition ease-in-out delay-50   hover:-translate-y-1 hover:scale-110  '},
+   {name:'Experience',link:'/Experience',cls:'h-fit rounded p-[0.5rem]  bg-black  m-[1rem]  w-[5rem] flex p-[0.6rem] items-center space-x-[1rem] w-fit   hover:shadow-lg shadow-md hover:shadow-[#d9d6d0] shadow-[#d9d6d0] transition ease-in-out delay-50   hover:-translate-y-1 hover:scale-110  '
+   },
+   {name:'Resume',link:'/Resume',cls:'h-fit rounded p-[0.5rem]  m-[1rem] bg-black  w-[5rem] flex p-[0.6rem] items-center space-x-[1rem] w-fit   hover:shadow-lg shadow-md hover:shadow-[#d9d6d0] shadow-[#d9d6d0] transition ease-in-out delay-50   hover:-translate-y-1 hover:scale-110  '},
+   {name:'Contacts',link:'/Contacts',cls:'h-fit rounded p-[0.5rem]  m-[1rem]  bg-black  w-[5rem] flex p-[0.6rem] items-center space-x-[1rem] w-fit   hover:shadow-lg shadow-md hover:shadow-[#d9d6d0] shadow-[#d9d6d0] transition ease-in-out delay-50   hover:-translate-y-1 hover:scale-110  '}]);
     
   return (
     <>
     <div className='fixed bottom-[0.5rem] animate-pulse text-white text-[1rem]  flex justify-center w-[98vw] max-md:overflow-auto z-[1] max-md:bottom-[0rem]  max-md:pl-[20rem] max-md:pt-[-3rem '>
     {buttons.map((x,index)=>(
      <Link href={x.link} key={index}>
-       <button className={x.cls } >
-         {x.name}  
-         <CiCircleChevDown />
-       </button> 
-     </Link>
+     <button className={x.cls } >
+       {x.name}  
+       <CiCircleChevDown className='ml-[1rem]'/>
+     </button> 
+   </Link>
       ))}
    </div>
     </>
